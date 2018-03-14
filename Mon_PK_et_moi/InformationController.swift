2514ee02f5c4189e8290683217169a24acbacb27
Hugo LECLER <hugo.lecler@igtp4-mac14.isim.intra>
2018-03-14 11:42:39 +0100

    added some stuff

--- Mon_PK_et_moi/InformationController.swift
+++ Mon_PK_et_moi/InformationController.swift
@@ -8,32 +8,86 @@
 
 import Foundation
 import UIKit
-/*
+import CoreData
+
 class InformationController: UIViewController {
-    
+  
     @IBOutlet weak var prenomField: UITextField!
     @IBOutlet weak var nomField: UITextField!
     @IBOutlet weak var ageField: UITextField!
     @IBOutlet weak var sexeField: UITextField!
+    var config : [Configuration] = []
+    
+    func saveprenom(firstname: String) {
+        
+        let context = PersistenceService.context
+        let config = Configuration(context: context)
+        config.prenomPatient = firstname
+        config.nomPatient = "test"
+        config.age = 10
+        config.sexePatient = "test"
+        do{
+        try context.save()
+        } catch let error as NSError{
+            self.alertError(errorMsg: "\(error)", userInfo : "\(error.userInfo)")
+            return
+        }
+    }
+    
+    func savenom(lastname: String) {
+        
+    }
+    
+    func saveage(age name: String) {
+        
+    }
+    
+    func savesexe(gender name: String) {
+        
+    }
+
+    func alertError(errorMsg error : String, userInfo user: String = ""){
+        let alert = UIAlertController(title : error, message : user, preferredStyle : .alert)
+        let cancelAction = UIAlertAction(title : "Ok", style : .default)
+        alert.addAction(cancelAction)
+        present(alert,animated: true)
+    }
     
     @IBAction func pressbutton(_ sender: Any) {
-        //let conf:Config = Config()
+        if (prenomField.text != nil){
+            saveprenom(firstname: prenomField.text!)
+        }
+        if (prenomField.text != nil){
+            savenom(lastname: nomField.text!)
+        }
+        if (prenomField.text != nil){
+            saveage(age: ageField.text!)
+        }
+        if (prenomField.text != nil){
+            savesexe(gender: sexeField.text!)
+        }
+        
+        
+        
+        
+                //let conf:Config = Config()
         // Vérification du champs nom
         //TODO
         // Enregistrement dans la config
         /*
         let appDel = UIApplication.shared.delegate as! AppDelegate
         conf.updateNom(newNom: nomField.text!, appDel: appDel)
-         */
+ 
         let newConfig = Configuration(context: PersistenceService.context)
         newConfig.nomPatient = nomField.text
         newConfig.prenomPatient = prenomField.text
         newConfig.sexePatient = sexeField.text
         newConfig.age = Int16(ageField.text!)!
         print("saving ... "+newConfig.nomPatient!+newConfig.prenomPatient!+newConfig.sexePatient!)
-        PersistenceService.saveContext()
+        PersistenceService.saveContext()*/
     }
     
     
+   
     
-}*/
+}
