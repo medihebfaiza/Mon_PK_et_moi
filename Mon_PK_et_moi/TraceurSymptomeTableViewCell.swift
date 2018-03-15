0dc22754f1e84b3df17acc0998b662a17c2cc278
Mohamed-Iheb FAIZA <mohamed-iheb.faiza@igtp4-mac01-6.isim.intra>
2018-03-15 18:31:07 +0100

    Add : Traceur de symptomes controlleur

---
+++ Mon_PK_et_moi/TraceurSymptomeTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  TraceurSymptomeTableViewCell.swift
+//  Mon_PK_et_moi
+//
+//  Created by Mohamed-Iheb FAIZA on 15/03/2018.
+//  Copyright © 2018 FAIZA&LECLER. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TraceurSymptomeTableViewCell:UITableViewCell {
+    @IBOutlet weak var heureLabel: UILabel!
+    @IBOutlet weak var symptomeLabel: UILabel!
+    
+}
