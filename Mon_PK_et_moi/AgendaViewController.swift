614922d1a713e119a5defd11aba3aa173f6237b7
Mohamed-Iheb FAIZA <mohamed-iheb.faiza@igtp4-mac01-6.isim.intra>
2018-03-16 11:12:18 +0100

    Edit : affichage des RDV sur l'agenda

--- Mon_PK_et_moi/AgendaViewController.swift
+++ Mon_PK_et_moi/AgendaViewController.swift
@@ -15,14 +15,14 @@ class AgendaViewController: UIViewController, UITableViewDelegate, UITableViewDa
     
     @IBOutlet weak var eventsTable: UITableView!
     
-    var events : [Evenement] = []
+    var rdvs : [Rendezvous] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         //self.seedEvents()
-        self.loadEvents()
+        self.loadRDVs()
     }
     
     override func didReceiveMemoryWarning() {
@@ -31,12 +31,12 @@ class AgendaViewController: UIViewController, UITableViewDelegate, UITableViewDa
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.events.count
+        return self.rdvs.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = self.eventsTable.dequeueReusableCell(withIdentifier: "eventCell", for:indexPath ) as! AgendaTableViewCell
-        cell.eventNameLabel.text = self.events[indexPath.row].libelle
+        cell.eventNameLabel.text =  "RDV avec Dr. " + (self.rdvs[indexPath.row].estdemandepar?.nom)!
         return cell
     }
     
@@ -62,11 +62,11 @@ class AgendaViewController: UIViewController, UITableViewDelegate, UITableViewDa
     func delete(eventWithIndex index: Int) -> Bool {
         guard let appDel = UIApplication.shared.delegate as? AppDelegate else{return false}
         let context = appDel.persistentContainer.viewContext
-        let event = events[index]
-        context.delete(event)
+        let rdv = rdvs[index]
+        context.delete(rdv)
         do {
             try context.save()
-            self.events.remove(at: index)
+            self.rdvs.remove(at: index)
             return true
         }
         catch let error as NSError{
@@ -89,12 +89,12 @@ class AgendaViewController: UIViewController, UITableViewDelegate, UITableViewDa
         }
     }
     
-    func loadEvents() {
+    func loadRDVs() {
         guard let appDel = UIApplication.shared.delegate as? AppDelegate else{return}
         let context = appDel.persistentContainer.viewContext
-        let request : NSFetchRequest<Evenement> = Evenement.fetchRequest()
+        let request : NSFetchRequest<Rendezvous> = Rendezvous.fetchRequest()
         do {
-            try self.events = context.fetch(request)
+            try self.rdvs = context.fetch(request)
         }
         catch let error as NSError{
             self.alertError(errorMsg : "\(error)", userInfo : "\(error.userInfo)")
