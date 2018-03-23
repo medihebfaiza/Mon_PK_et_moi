695bc77b705c21ba19d853c3c1f11f6cd6d6aaad
Hugo LECLER <hugo.lecler@igtp4-mac14.isim.intra>
2018-03-23 10:01:33 +0100

    Added FinishedActivityController

---
+++ Mon_PK_et_moi/FinishedActivityViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FinishedActivityViewController.swift
+//  Mon_PK_et_moi
+//
+//  Created by Hugo LECLER on 23/03/2018.
+//  Copyright © 2018 FAIZA&LECLER. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class FinishedActivityViewController: UIViewController{
+
+    @IBAction func doneButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    };
+}
+
+
