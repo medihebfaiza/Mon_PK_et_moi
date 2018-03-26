37d3b4bd4ddd5745952f382a9ef66a6db34e275c
Mohamed-Iheb FAIZA <mohamed-iheb.faiza@igtp4-mac01-6.isim.intra>
2018-03-26 15:06:04 +0200

    add : Radio buttons for quiz

--- Mon_PK_et_moi/QuizViewController.swift
+++ Mon_PK_et_moi/QuizViewController.swift
@@ -13,5 +13,39 @@ import CoreData
 
 class QuizViewController : UIViewController{
 
-
+    @IBOutlet weak var radio1: RadioButton!
+    @IBOutlet weak var radio2: RadioButton!
+    @IBOutlet weak var radio3: RadioButton!
+    @IBOutlet weak var radio4: RadioButton!
+    @IBOutlet weak var radio5: RadioButton!
+    /*
+    lazy var radioButtons: [RadioButton] = {
+        return [
+            self.radioButton1,
+            self.radioButton2,
+            ]
+    }()
+    
+    
+    @IBAction func onRadioButton1Clicked(_ sender: RadioButton) {
+        updateRadioButton(sender)
+    }
+    @IBAction func onRadioButton2Clicked(_ sender: RadioButton) {
+        updateRadioButton(sender)
+    }
+    
+    
+    // MAIN Methodes
+    func updateRadioButton(_ sender: RadioButton){
+        radioButtons.forEach { $0.isSelected = false }
+        sender.isSelected = !sender.isSelected
+        
+    }
+    
+    func getSelectedRadioButton() -> RadioButton? {
+        var radioButton: RadioButton?
+        radioButtons.forEach { if($0.isSelected){ radioButton =  $0 } }
+        return radioButton
+    }*/
+    
 }
