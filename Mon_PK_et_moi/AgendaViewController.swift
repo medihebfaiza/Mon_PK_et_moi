7f1e9423951c60614506bcc1700b5c8d1103cbe7
Hugo LECLER <hugo.lecler@igtp4-mac14.isim.intra>
2018-03-26 14:29:35 +0200

    solved wrong datepicker starting date in AgendaController

--- Mon_PK_et_moi/AgendaViewController.swift
+++ Mon_PK_et_moi/AgendaViewController.swift
@@ -26,6 +26,17 @@ class AgendaViewController: UIViewController, UITableViewDelegate, UITableViewDa
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let gregorian = Calendar(identifier: .gregorian)
+        let now = Date()
+        var components = gregorian.dateComponents([.year, .month, .day, .hour, .minute, .second], from: now)
+        
+        // Change the time to 9:30:00 in your locale
+        components.hour = 0
+        components.minute = 0
+        components.second = 0
+        
+        let date = gregorian.date(from: components)!
+        filterDatePicker.date = date
         self.loadRDVs()
         filterDatePicker.addTarget(self, action: #selector(AgendaViewController.datePickerValueChanged), for: UIControlEvents.valueChanged)
     }
